5d92824bc5f35c85f092a76b4c1df997a373d449
Hasan Hasanzade <101423091+hasan-hasanzade@users.noreply.github.com>
2024-07-12 18:35:48 +0400

    header deleted

--- src/components/sign-in/index.jsx
+++ src/components/sign-in/index.jsx
@@ -1,12 +1,10 @@
 import FooterFive from "@/src/layout/footers/footer-5";
-import HeaderSeven from "@/src/layout/headers/header-7"; 
 import React from "react";
 import SingnInArea from "./singn-in-area";
 
 const SignIn = () => {
   return (
     <>
-      <HeaderSeven />
       <SingnInArea />
       <FooterFive style_contact={true} style_team={true} />
     </>
