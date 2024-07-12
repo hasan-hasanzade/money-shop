e5eeb19b76a102a54e39e3ada7b7a8c6d1f6f82a
Hasan Hasanzade <101423091+hasan-hasanzade@users.noreply.github.com>
2024-07-12 18:40:13 +0400

    project added

---
+++ src/components/project/index.jsx
@@ -0,0 +1,14 @@
+import BreadcrumbSeven from "@/src/common/breadcrumbs/breadcrumb-7";
+import React from "react";
+
+const Project = () => {
+  return (
+    <>
+      <main>
+        <BreadcrumbSeven />
+      </main>
+    </>
+  );
+};
+
+export default Project;
